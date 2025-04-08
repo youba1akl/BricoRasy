e65ddcc35431a64556a0047dabffa4fb9bc9a8a1
aklil youba <youbaklil232004@gmail.com>
2025-04-08 18:47:23 +0100

    Modification de la page add annonce

--- lib/screens/add_anno/add-anno-screen.dart
+++ lib/screens/add_anno/add-anno-screen.dart
@@ -11,121 +11,164 @@ class _AddAnnoScreenState extends State<AddAnnoScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-               backgroundColor: const Color.fromARGB(201, 118, 117, 117), 
-
-        centerTitle: true,
-        title: Text(
-          
-          "Add Annonce",
-          style: TextStyle(
-            
-            color: Color.fromARGB(255, 16, 25, 62),
-            fontWeight: FontWeight.bold,
-            fontSize: 40,
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 80,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(242, 248, 248, 248),
+              ),
+              padding: EdgeInsets.only(bottom: 15),
+              child: const Text(
+                'Add Annonce',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
           ),
-        ),
-      ),
-      body: Container(
-        color: const Color.fromARGB(201, 118, 117, 117),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(19),
-          child: Container(
-             color: Color(0xFFF5F5F5), 
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
+          Positioned(
+            top: 60,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 20,
+                    spreadRadius: 0,
+                    offset: Offset(0, -5),
+                  ),
+                ],
+              ),
               child: GridView.count(
                 crossAxisCount: 2,
                 crossAxisSpacing: 12,
                 mainAxisSpacing: 12,
                 children: [
-                  Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    elevation: 8,
-                    child: InkWell(
-                      borderRadius: BorderRadius.circular(15),
+                  Expanded(
+                    child: GestureDetector(
                       onTap: () {},
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          ImageIcon(
-                            const AssetImage('assets/images/logo-google.png'),
-                            size: 80,
-                            color: Colors.blueAccent,
-                          ),
-                          const SizedBox(height: 10),
-                          const Text(
-                            "Briclole",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Color.fromARGB(255, 6, 50, 226),
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 20,
+                              spreadRadius: 0,
+                              offset: Offset(0, 0),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Icon(Icons.construction_rounded, size: 70),
+                            const SizedBox(height: 10),
+                            Text(
+                              "Briclole",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                  Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    elevation: 8,
-                    child: InkWell(
-                      borderRadius: BorderRadius.circular(15),
+                  Expanded(
+                    child: GestureDetector(
                       onTap: () {},
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          ImageIcon(
-                            const AssetImage('assets/images/logo-google.png'),
-                            size: 80,
-                            color: Colors.blueAccent,
-                          ),
-                          const SizedBox(height: 10),
-                          const Text(
-                            "Professionnel",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Color.fromARGB(255, 6, 50, 226),
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 20,
+                              spreadRadius: 0,
+                              offset: Offset(0, 0),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Icon(Icons.business_center, size: 70),
+                            const SizedBox(height: 10),
+                            Text(
+                              "Professionel",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                  Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    elevation: 8,
-                    child: InkWell(
-                      borderRadius: BorderRadius.circular(15),
+                  Expanded(
+                    child: GestureDetector(
                       onTap: () {},
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          ImageIcon(
-                            const AssetImage('assets/images/logo-google.png'),
-                            size: 80,
-                            color: Colors.blueAccent,
-                          ),
-                          const SizedBox(height: 10),
-                          const Text(
-                            "Article",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Color.fromARGB(255, 6, 50, 226),
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 20,
+                              spreadRadius: 0,
+                              offset: Offset(0, 0),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Icon(Icons.format_paint, size: 70),
+                            const SizedBox(height: 10),
+                            Text(
+                              "Objet",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
@@ -133,7 +176,7 @@ class _AddAnnoScreenState extends State<AddAnnoScreen> {
               ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
