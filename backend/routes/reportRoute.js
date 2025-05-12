712d0ec0c36fb30906161ccfd652b18ff06abdba
sidali <sidalialili694@gmail.com>
2025-05-12 08:39:38 +0100

    ct

--- backend/routes/reportRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const reportController = require('../controllers/reportController');
-
-router.post('/reports', reportController.createReport);
-
-module.exports = router;
